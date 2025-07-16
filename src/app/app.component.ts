77f315a46369bb3e8575919868f3f2d4f2bef165
LAGM <luisiyi1997@gmail.com>
2025-07-15 21:45:49 -0700

    Routes and redirection in Angular
    -first we defined the routes array in the "app.routes.ts" file, this case we just set the /productos route by default and if the user set empty after the base url, the system will redirect to the "/productos" anyway.
    -the "RouterModule" is very IMPORTANT because angular cannot recognize and manage the routing without this also cannot interpret "<router-outlet>" and the "routerLink", so we import "RouterModule" in the "app.component.ts" file instead calling the components directly.
    -with all this set before, now we can route the "app.component.html" file, we replace the (href="") with (routerLink="") we replace it because href recharge the page completly and lost the app´s status, (routerLinkActive="active") is for make a highligh in the active tab.
    -<router-outlet> is for rendering dinamically the components based on the current route in the URL.
    - for routing works we need define the "routes" in the "app.config.ts", although this setup is by default. "provideRouter(routes)" this enable the routes in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ProductoListaComponent } from "./producto-lista/producto-lista.component";
+import { RouterOutlet } from "../../node_modules/@angular/router/index";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [ProductoListaComponent]
+  imports: [RouterModule]
 })
 export class AppComponent {
   title = 'sistema-inventario-ang-app';
