feca69e8f1d06052eee73e95e26a39241537679d
LAGM <luisiyi1997@gmail.com>
2025-07-19 20:43:57 -0700

    Agregar Producto service beside in the component ts
    summary: first service, then component, and finally the html view.
    _____**SERVICE:
    -added the "agregarProducto(producto: Producto): Observable<Object>" method in the "producto.service.ts" class, this we use the "clienteHttp" to post to the baseURL the producto received in the method, this method return the Observable object.
    _____**COMPONENT:
    -updated the "agregar-producto.component.ts" file, added the producto attribute and initializated with new Producto();, then injected the "productoServicio" from the "ProductoService", also injected the "enrutador" from the "Router", also added the "onSubmit" method
    -added the "guardarProducto" method, we subscribe here from our service, this method receive the "producto" recently created and intializated in the previous point mentioned, this method redirect to the "/productos" thankfully for the "irListaProductos" method.
    in this component is very IMPORTANT to have "FormsModule" in the array imports because this allows "two way data binding[ngModel]" in Angular between the component and the view.
    _____**HTML/VIEW:
    -added the (ngSubmit)=on"onSubmit()" in the form from the "agregar-producto.component.html" file this avoid recharge the page and instead runs the "onSubmit" method in the "component", also we added the "name" property and "[(ngModel)]="producto.property" in each input because this links each input with the object´s property, allowing the changes in the form directly in the variable.

--- src/app/agregar-producto/agregar-producto.component.ts
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 //ng g c agregar-producto --skip-tests
 @Component({
   selector: 'app-agregar-producto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], //Two way data binding
   templateUrl: './agregar-producto.component.html'
 })
 export class AgregarProductoComponent {
+  producto: Producto = new Producto();
+
+  private productoServicio = inject(ProductoService);
+  private enrutador = inject(Router);
+
+  onSubmit(){
+    this.guardarProucto();
+  }
+
+  guardarProucto(){
+    this.productoServicio.agregarProducto(this.producto).subscribe({
+      next: (datos) => {
+        this.irListaProductos();
+      },
+      error: (error: any) => {console.log(error)}
+    });
+  }
+
+  irListaProductos(){
+    this.enrutador.navigate(['/productos']);
+  }
 
 }
--- src/app/producto.service.ts
+++ src/app/producto.service.ts
@@ -17,4 +17,8 @@ export class ProductoService {
     return this.clienteHttp.get<Producto[]>(this.urlBase)
   }
 
+  agregarProducto(producto: Producto): Observable<Object>{
+    return this.clienteHttp.post(this.urlBase, producto);
+  }
+
 }
