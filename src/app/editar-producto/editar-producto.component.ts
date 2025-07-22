ade0abcbeb0ac37b001c45908dd8a496fee13766
LAGM <luisiyi1997@gmail.com>
2025-07-21 20:58:41 -0700

    Editar Producto component added
    -added "editar-producto" component with this command "ng g c editar-producto --skip-tests", we deleted the "editar-producto.component.css" file because we do not need it, also we deleted the link in the "ts" file.
    -added the new route in the "app-routes.ts" file called "editar-producto/:id", this one will redirect to the "EditarProductoComponent" class/file.
    -updated the "producto-lista" html/view, we added the new column called "Acciones" it has an "editar" button, this one has a method called "editarProducto" that receive the producto´s id.
    -updated the "producto-lista" ts, we added the "editarProducto" method that receive the id, and this one will redirect to the "editar-producto" component and we send it the id, to be possible this, we could to make a dependency injection with (Router).

---
+++ src/app/editar-producto/editar-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-producto',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-producto.component.html'
+})
+export class EditarProductoComponent {
+
+}
--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Producto } from '../producto';
 import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-lista',
@@ -12,6 +13,7 @@ export class ProductoListaComponent {
   productos!: Producto[];
 
   private productoServicio = inject(ProductoService);
+  private enrutador = inject(Router);
 
   ngOnInit(){
     //Cargar los productos
@@ -29,5 +31,9 @@ export class ProductoListaComponent {
         }
       }
     );
+  }
+
+  editarProducto(id: number){
+    this.enrutador.navigate(['editar-producto', id]);
   }
 }
